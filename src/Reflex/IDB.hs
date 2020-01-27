b493b0e581be510c345a8d80c1af8abbd412a85d
Greg Hale <imalsogreg@gmail.com>
2020-01-26 21:57:16 -0500

    add syncs and debugging

--- src/Reflex/IDB.hs
+++ src/Reflex/IDB.hs
@@ -15,46 +15,55 @@
 
 module Reflex.IDB where
 
+import           Control.Concurrent.STM            (atomically)
+import qualified Control.Concurrent.STM.TMVar      as STM
 import           Control.Monad
 import           Control.Monad.IO.Class
 import           Control.Monad.Ref
-import           Control.Monad.Trans.Class  (MonadTrans, lift)
+import           Control.Monad.Trans.Class         (MonadTrans, lift)
 import           Control.Monad.Trans.Except
 import           Control.Monad.Trans.Free
 import           Control.Monad.Trans.Reader
-import           Control.Monad.Trans.State  hiding (get)
-import           Data.Aeson                 as A
-import           Data.ByteString            (ByteString)
-import qualified Data.ByteString            as BS
+import           Control.Monad.Trans.State         hiding (get)
+import           Data.Aeson                        as A
+import           Data.ByteString                   (ByteString)
+import qualified Data.ByteString                   as BS
 import           Data.Default
-import           Data.Dependent.Map         (DSum (..))
+import           Data.Dependent.Map                (DSum (..))
 import           Data.Functor.Identity
 import           Data.IORef
-import           Data.Maybe                 (fromMaybe)
+import           Data.Maybe                        (fromMaybe)
 import           Data.Proxy
-import           Data.Text                  (Text)
-import qualified Data.Text                  as T
+import           Data.Text                         (Text)
+import qualified Data.Text                         as T
 import           Data.Word
 import           GHC.TypeLits
 import           GHCJS.DOM
-import qualified GHCJS.DOM.Enums            as DOM
+import qualified GHCJS.DOM.Enums                   as DOM
 import           GHCJS.DOM.EventM
-import qualified GHCJS.DOM.IDBCursor        as IDBCurs
-import qualified GHCJS.DOM.IDBDatabase      as IDBD
-import qualified GHCJS.DOM.IDBFactory       as IDBF
-import qualified GHCJS.DOM.IDBKeyRange      as IDBKeyRan
-import qualified GHCJS.DOM.IDBObjectStore   as IDBStore
-import qualified GHCJS.DOM.IDBOpenDBRequest as IDBOReq
-import qualified GHCJS.DOM.IDBRequest       as IDBReq
-import qualified GHCJS.DOM.IDBTransaction   as IDBTrans
-import qualified GHCJS.DOM.Types            as DOM
-import qualified GHCJS.DOM.Window           as FFIWin
-import           Prelude                    hiding ((!!))
+import qualified GHCJS.DOM.IDBCursor               as IDBCurs
+import qualified GHCJS.DOM.IDBDatabase             as IDBD
+import qualified GHCJS.DOM.IDBFactory              as IDBF
+import qualified GHCJS.DOM.IDBKeyRange             as IDBKeyRan
+import qualified GHCJS.DOM.IDBObjectStore          as IDBStore
+import qualified GHCJS.DOM.IDBOpenDBRequest        as IDBOReq
+import qualified GHCJS.DOM.IDBRequest              as IDBReq
+import qualified GHCJS.DOM.IDBTransaction          as IDBTrans
+import qualified GHCJS.DOM.Types                   as DOM
+import qualified GHCJS.DOM.Window                  as FFIWin
+import           Prelude                           hiding ((!!))
 import           Reflex
 import           Reflex.Dom.Class
 import           Reflex.Dom.Core
 import           Reflex.Host.Class
 
+-- tmp
+import           Control.Lens                      ((^.))
+import qualified GHCJS.DOM                         as DOM
+import           GHCJS.DOM.Types                   (MonadJSM, liftJSM)
+import qualified GHCJS.DOM.Window                  as Window
+import qualified Language.Javascript.JSaddle       as JSaddle
+import qualified Language.Javascript.JSaddle.Types as JSaddle
 
 
 -- | Tag the 'Nothing' value of a 'Maybe'
@@ -363,6 +372,7 @@ interpret input idbTrans (OpOpenStore storeN f) = do
 interpret input idbTrans (OpAdd (ObjectStore store) item key f) = do
       liftIO $ print $ "adding " ++ (show key)
       req <- IDBStore.add store item key
+      liftJSM syncPoint
       f
 interpret input idbTrans (OpClear (ObjectStore store) f) = do
       req <- IDBStore.clear store
@@ -388,11 +398,38 @@ interpret input idbTrans (OpCount (ObjectStore store) key f) = do
             Only k -> do
               range <- IDBKeyRan.only k
               IDBStore.countRange store (Just range)
-      f (Left $ T.pack "TODO")
+      f (Left $ T.pack "TODOS")
 interpret input idbTrans (OpGet (ObjectStore store) key f) = do
+      consoleLog $ T.pack "idb Get handler"
+      alert $ T.pack "idb Get handler"
       keyVal <- DOM.liftJSM $ DOM.toJSVal key
+      consoleLog keyVal
+      alert (T.pack ("key: " ++ show key))
+      resultVar <- liftIO $ atomically $ STM.newEmptyTMVar
+      consoleLog $ T.pack "created Get tmvar"
       req <- IDBStore.get store key
-      f (Left $ T.pack "TODO")
+      consoleLog $ T.pack "created request"
+      DOM.liftJSM $ on req IDBReq.success $ do
+        consoleLog $ T.pack "in get success handler"
+
+        -- r <- IDBReq.getResultUnchecked (req :: Int)
+        r <- IDBReq.getResult req
+        consoleLog $ T.pack "called getResult, got: "
+        r' <- case r of
+          Nothing -> consoleLog (T.pack "fail after success") >> alert (T.pack "fail after success") >> return (Left $ T.pack "getResult failed after success event")
+          Just reqResult -> do
+            liftJSM syncPoint
+            consoleLog $ T.pack "get success handler got Just reqResult"
+            DOM.liftJSM $ fmap Right $ DOM.fromJSVal =<< DOM.toJSVal reqResult
+
+        consoleLog $ T.pack "about to putTMVar"
+        alert $ T.pack "about to put TMVar"
+        liftIO $ atomically $ STM.putTMVar resultVar r'
+        consoleLog $ T.pack "finished putTMVar"
+        alert $ T.pack "finished putTMVar"
+
+      consoleLog "registered get handler"
+      liftIO (atomically (STM.readTMVar resultVar)) >>= \res -> consoleLog "f res" >> f res
 interpret input idbTrans (OpGetAll (ObjectStore store) key kcount f) = do
   error "getAll is not available in GHCJS"
 interpret input idbTrans (OpGetAllKeys (ObjectStore store) key kcount f) = do
@@ -451,3 +488,18 @@ interpretCursor curs (OpContinue f) = do
 
 getInput :: Monad m => IDB t r m r
 getInput = IDB ((lift . lift) ask)
+
+consoleLog :: (JSaddle.ToJSVal a, MonadJSM m) => a -> m ()
+consoleLog v = liftJSM $ do
+  w <- JSaddle.jsg ("console" :: String)
+  jsv <- JSaddle.toJSVal v
+  w ^. JSaddle.js1 ("log" :: String) jsv
+  return ()
+
+
+
+
+alert :: (MonadJSM m) => T.Text -> m ()
+alert txt = do
+  win <- DOM.currentWindowUnchecked
+  liftJSM (Window.alert win txt)
